d6c47f5156ac61fb774c769b17b6fc4c657ad433
pom.wang <wanglei134@foxmail.com>
2014-11-29 00:30:17 +0800

    Design Mode

---
+++ src/不见不散.java
@@ -0,0 +1,13 @@
+
+public class 不见不散 {
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while(true)
+		{
+			System.out.println("奉天承运,仙姑诏曰:六月二一,不见不散,七点入场,七点过半,民主湖畔,报告厅内,正式启幕,节目精彩,观众可爱,仙姑你好,仙姑再见!");
+		}
+	}
+}
