0fc82ce2dc63bb76cf50c1dc50fc360074377399
pomwang <wanglei134@foxmail.com>
2014-11-29 20:58:56 +0800

    update part05

---
+++ src/part04_Control/Control.java
@@ -0,0 +1,18 @@
+package part04_Control;
+
+import java.util.Random;
+
+public class Control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//foreach is very import 不用担心越界问题
+		float []a=new float[10];
+		for(int i=0;i<10;i++)
+			a[i]=new Random().nextFloat();
+		for (float f : a) {
+			System.out.println(f);
+		}
+	}
+
+}
