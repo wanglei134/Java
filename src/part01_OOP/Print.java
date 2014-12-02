b7e933e580546294f38e638783aed86aade9d5b4
pomwang <wanglei134@foxmail.com>
2014-12-02 19:06:41 +0800

    delete print

--- src/part01_OOP/Print.java
+++ src/part01_OOP/Print.java
@@ -4,7 +4,4 @@ public class Print {
 	public static void println(Object obj){
 		System.out.println(obj);
 	}
-	public static void print(Object obj){
-		System.out.print(obj);
-	}
 }
---
+++ src/part09_Interface/Apply.java
@@ -0,0 +1,19 @@
+package part09_Interface;
+
+import part01_OOP.Print;
+
+class Processor{
+	public String name(){
+		return getClass().getSimpleName();
+		//return the simple name of the underlying class
+	}
+}
+public class Apply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Print.println("");
+
+	}
+
+}
