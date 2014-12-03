57abe90ff04efd915f8e8d4fd41fb03a4d7b986a
wanglei134 <GQC347@gqc347-01.ds.mot.com>
2014-12-03 13:21:09 +0800

    inner class parcel1

---
+++ src/part10_InnerClass/Parcel1.java
@@ -0,0 +1,37 @@
+package part10_InnerClass;
+
+import part01_OOP.Print;
+
+public class Parcel1 {
+	class Contents{
+		private int i=11;
+		public int value(){
+			return this.i;
+		}
+	}
+	class Destination{
+		private String label;
+		public Destination(String whereTo) {
+			// TODO Auto-generated constructor stub
+			this.label=whereTo;
+		}
+		public String readLabel(){
+			return this.label;
+		}
+	}
+	public void ship(String dest){
+		Contents c=new Contents();
+		Print.println(c.i);
+		Destination d=new Destination(dest);
+		Print.println(d.readLabel());
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Parcel1 p=new Parcel1();
+		p.ship("chongqing");
+//		11
+//		chongqing
+
+	}
+
+}
