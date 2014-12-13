7faa3e9c6af981bb8d1c4b078d4b5b5a2935eaab
pomwang <wanglei134@foxmail.com>
2014-12-13 10:05:47 +0800

    usb4java

---
+++ src/demo/demo01.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.usb4java.javax.Services;
+
+public class demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
